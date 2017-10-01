f56b65d4864d66048bfaddfa549303758eb83c92
Mike Greiner <mike.greiner@gmail.com>
2017-10-01 12:46:30 -0600

    Move to my own repo and Firebase project

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,32 +1,27 @@
-import React, { PropTypes } from 'react';
-import classNames from 'classnames';
-import './Message.css';
+import React, { PropTypes } from 'react'
+import classNames from 'classnames'
+import './Message.css'
 
 const Message = ({ belongsToCurrentUser, content, id, isAdmin, user, deleteMessage }) => (
   <article className={classNames('Message', { 'current-user': belongsToCurrentUser })}>
-    <div className="Message--avatar">
-      <img
-        role="presentation"
-        src={user.photoURL}
-      />
-    </div>
+    {user && (
+      <div className="Message--avatar">
+        <img role="presentation" src={user.photoURL} />
+      </div>
+    )}
     <div className="Message--main">
-      <h4 className="Message--user">{ user.displayName }</h4>
-      <p className="Message--content">{ content }</p>
+      {user && <h4 className="Message--user">{user.displayName}</h4>}
+      <p className="Message--content">{content}</p>
       <footer className="Message--controls">
-        {
-          (belongsToCurrentUser || isAdmin) &&
-          <button
-            className="small destructive"
-            onClick={deleteMessage}
-          >
+        {(belongsToCurrentUser || isAdmin) && (
+          <button className="small destructive" onClick={deleteMessage}>
             Delete
           </button>
-        }
+        )}
       </footer>
     </div>
   </article>
-);
+)
 
 Message.propTypes = {
   belongsToCurrentUser: PropTypes.bool,
@@ -35,6 +30,6 @@ Message.propTypes = {
   id: PropTypes.string,
   timeStamp: PropTypes.number,
   user: PropTypes.object,
-};
+}
 
-export default Message;
+export default Message
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,25 @@
-import firebase from 'firebase';
+import firebase from 'firebase'
 
 const config = {
-  apiKey: 'AIzaSyAhWMYWhBG2UvsNMZ-qVLbaqVZ5aTzwsNU',
-  authDomain: 'hot-takes.firebaseapp.com',
-  databaseURL: 'https://hot-takes.firebaseio.com',
-  storageBucket: 'hot-takes.appspot.com',
-  messagingSenderId: '823384132241'
-};
+  // MEG
+  apiKey: 'AIzaSyC_WFcjDVzQu8yrrp1rmmSwqekgZZk5Xac',
+  authDomain: 'hot-takes-eed63.firebaseapp.com',
+  databaseURL: 'https://hot-takes-eed63.firebaseio.com',
+  projectId: 'hot-takes-eed63',
+  storageBucket: '',
+  messagingSenderId: '728070853518',
+  // Steve:
+  // apiKey: 'AIzaSyAhWMYWhBG2UvsNMZ-qVLbaqVZ5aTzwsNU',
+  // authDomain: 'hot-takes.firebaseapp.com',
+  // databaseURL: 'https://hot-takes.firebaseio.com',
+  // storageBucket: 'hot-takes.appspot.com',
+  // messagingSenderId: '823384132241'
+}
 
-firebase.initializeApp(config);
+firebase.initializeApp(config)
 
-export default firebase;
+export default firebase
 
-export const database = firebase.database();
-export const auth = firebase.auth();
-export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+export const database = firebase.database()
+export const auth = firebase.auth()
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
